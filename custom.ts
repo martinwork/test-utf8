4e64fa2e7de2f1586186fcd47488cfc7707ac65d
Martin Williams <martinwork@users.noreply.github.com>
2024-07-30 00:45:24 +0100

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,22 @@
+
+/**
+* Use this file to define custom functions and blocks.
+* Read more at https://makecode.microbit.org/blocks/custom
+*/
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace custom {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block
+    export function utf8(s: String): String {
+        return s
+    }
+}
